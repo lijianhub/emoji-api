a9d80a176c638b7a63138d4d9ba75453f5829772
Nathan Daniel Peterson <natperson@gmail.com>
2017-12-28 19:53:41 -0800

    work on emoji db

--- models/emojiModel.js
+++ models/emojiModel.js
@@ -4,9 +4,8 @@ const db = require('../db/knex.js')
 class emojiModel {
     constructor(){}
     static getAll(){
-        console.log('i am in the model')
         return db('emoji')
-    }
+    }        
 }
 
 module.exports = emojiModel
--- routers/emojiRouter.js
+++ routers/emojiRouter.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const emojiController = require('../controllers/emojiController.js')
 
 router.get('/', emojiController.getAll)
+// Use level in url to get emoji from that level
+
 
 module.exports = router
